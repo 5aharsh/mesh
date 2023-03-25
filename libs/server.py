30ac7f48476b085fbb3e1ec6b6d4fa1bed346eda
Saharsh Anand <saharshpro@hotmail.com>
2023-03-25 17:56:38 +0530

    Ditching win32api

--- libs/server.py
+++ libs/server.py
@@ -4,11 +4,11 @@ import posixpath
 import html
 import sys
 import io
-import win32api
 
 from http import HTTPStatus
 from http.server import SimpleHTTPRequestHandler
 from .assetLoader import template, preScript, postScript, style
+
 
 class FilerHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, dir='/'):
@@ -21,7 +21,7 @@ class FilerHandler(SimpleHTTPRequestHandler):
         self.dirpath = self.translate_path(self.path)
         if os.path.isfile(self.dirpath):
             process = os.system('"{0}"'.format(self.dirpath))
-            redirect_path = '/'.join(self.path.split('/')[:-1])+'/'
+            redirect_path = '/'.join(self.path.split('/')[:-1]) + '/'
             self.send_response(HTTPStatus.MOVED_PERMANENTLY)
             self.send_header('Location', redirect_path)
             self.end_headers()
@@ -39,8 +39,8 @@ class FilerHandler(SimpleHTTPRequestHandler):
         to serve multiple drives
         """
         # abandon query parameters
-        path = path.split('?',1)[0]
-        path = path.split('#',1)[0]
+        path = path.split('?', 1)[0]
+        path = path.split('#', 1)[0]
         # Don't forget explicit trailing slash when normalizing. Issue17324
         trailing_slash = path.rstrip().endswith('/')
         try:
@@ -52,7 +52,7 @@ class FilerHandler(SimpleHTTPRequestHandler):
         if trailing_slash:
             path += '/'
         return path
-    
+
     def list_directory(self, path):
         """Helper to produce a directory listing (absent index.html).
 
@@ -61,7 +61,7 @@ class FilerHandler(SimpleHTTPRequestHandler):
         interface the same as for send_head().
 
         """
-        if path=='/':
+        if path == '/':
             try:
                 encoded = self.list_drives()
             except OSError:
@@ -69,7 +69,8 @@ class FilerHandler(SimpleHTTPRequestHandler):
                     HTTPStatus.NOT_FOUND,
                     "No permission to list directory")
                 return None
-        elif path=='/search':
+        elif path.startswith('/search'):
+            print(path)
             try:
                 encoded = self.list_search(path)
             except OSError:
@@ -94,7 +95,7 @@ class FilerHandler(SimpleHTTPRequestHandler):
         self.send_header("Content-Length", str(len(encoded)))
         self.end_headers()
         return f
-    
+
     def list_dir(self, path):
         list = os.listdir(path)
         list.sort(key=lambda a: a.lower())
@@ -118,79 +119,80 @@ class FilerHandler(SimpleHTTPRequestHandler):
             if os.path.islink(fullname):
                 classname = "folder-item"
                 # Note: a link to a directory displays with @ and links with /
-            r.append('<a href="%s" title="%s" meta-name="%s" class="directory-link"><li class="directory-item %s">%s</li></a>'
-                        % (
-                            urllib.parse.quote(linkname, errors='surrogatepass'),
-                            html.escape(displayname, quote=False),
-                            html.escape(displayname, quote=False),
-                            classname, 
-                            html.escape(displayname, quote=False)
-                        )
-                    )
+            r.append(
+                '<a href="%s" title="%s" meta-name="%s" class="directory-link"><li class="directory-item %s">%s</li></a>'
+                % (
+                    urllib.parse.quote(linkname, errors='surrogatepass'),
+                    html.escape(displayname, quote=False),
+                    html.escape(displayname, quote=False),
+                    classname,
+                    html.escape(displayname, quote=False)
+                )
+                )
         r.append('</ul>')
         app = '\n'.join(r)
         navurl = "/"
         navitem = []
         navitem.append('<a href="%s" class="navpath">%s</a>'
-            % (
-                navurl,
-                "Home"
-            )
-        )
+                       % (
+                           navurl,
+                           "Home"
+                       )
+                       )
         for i in displaypath.split("/")[1:]:
-            navurl+="%s/" % (i)
+            navurl += "%s/" % (i)
             navitem.append('<a href="%s" class="navpath">%s</a>'
-                    % (
-                        navurl,
-                        i
-                    )
-                )
+                           % (
+                               navurl,
+                               i
+                           )
+                           )
         navhead = " &#x276D; ".join(navitem)
         title = " &#x276D; ".join(displaypath.split("/")[1:])
         page = template.format(
-            style, 
-            preScript, 
-            app, 
+            style,
+            preScript,
+            app,
             postScript,
             navhead,
             title
         )
         encoded = page.encode(enc, 'surrogateescape')
         return encoded
-    
+
     def list_drives(self):
-        drives = win32api.GetLogicalDriveStrings()
-        drives = drives.split('\\\000')[:-1]
+        drives = [chr(x) + ":" for x in range(65, 91) if os.path.exists(chr(x) + ":")]
         r = []
         enc = sys.getfilesystemencoding()
         r.append('<ul class="directory-list">')
         for drive in drives:
             classname = "drive-item"
-            linkname = drive+"/"
-                # Note: a link to a directory displays with @ and links with /
-            r.append('<a href="%s" title="%s" meta-id="%s"class="directory-link"><li class="directory-item %s">%s</li></a>'
-                        % (
-                            urllib.parse.quote(linkname, errors='surrogatepass'),
-                            drive,
-                            hash(drive),
-                            classname, 
-                            drive
-                        )
-                    )
+            linkname = drive + "/"
+            # Note: a link to a directory displays with @ and links with /
+            r.append(
+                '<a href="%s" title="%s" meta-id="%s"class="directory-link"><li class="directory-item %s">%s</li></a>'
+                % (
+                    urllib.parse.quote(linkname, errors='surrogatepass'),
+                    drive,
+                    hash(drive),
+                    classname,
+                    drive
+                )
+                )
         r.append('</ul>')
         app = '\n'.join(r)
         title = "Home"
         page = template.format(
-            style, 
-            preScript, 
-            app, 
+            style,
+            preScript,
+            app,
             postScript,
             title,
             title
         )
         encoded = page.encode(enc, 'surrogateescape')
         return encoded
-    
+
     def list_search(self, path):
         term = urllib.parse.parse_qs(urllib.parse.urlparse(path))["q"]
         list = os.listdir(path)
@@ -215,39 +217,40 @@ class FilerHandler(SimpleHTTPRequestHandler):
             if os.path.islink(fullname):
                 classname = "folder-item"
                 # Note: a link to a directory displays with @ and links with /
-            r.append('<a href="%s" title="%s" meta-name="%s" class="directory-link"><li class="directory-item %s">%s</li></a>'
-                        % (
-                            urllib.parse.quote(linkname, errors='surrogatepass'),
-                            html.escape(displayname, quote=False),
-                            html.escape(displayname, quote=False),
-                            classname, 
-                            html.escape(displayname, quote=False)
-                        )
-                    )
+            r.append(
+                '<a href="%s" title="%s" meta-name="%s" class="directory-link"><li class="directory-item %s">%s</li></a>'
+                % (
+                    urllib.parse.quote(linkname, errors='surrogatepass'),
+                    html.escape(displayname, quote=False),
+                    html.escape(displayname, quote=False),
+                    classname,
+                    html.escape(displayname, quote=False)
+                )
+                )
         r.append('</ul>')
         app = '\n'.join(r)
         navurl = "/"
         navitem = []
         navitem.append('<a href="%s" class="navpath">%s</a>'
-            % (
-                navurl,
-                "Home"
-            )
-        )
+                       % (
+                           navurl,
+                           "Home"
+                       )
+                       )
         for i in displaypath.split("/")[1:]:
-            navurl+="%s/" % (i)
+            navurl += "%s/" % (i)
             navitem.append('<a href="%s" class="navpath">%s</a>'
-                    % (
-                        navurl,
-                        i
-                    )
-                )
+                           % (
+                               navurl,
+                               i
+                           )
+                           )
         navhead = " &#x276D; ".join(navitem)
         title = " &#x276D; ".join(displaypath.split("/")[1:])
         page = template.format(
-            style, 
-            preScript, 
-            app, 
+            style,
+            preScript,
+            app,
             postScript,
             navhead,
             title
